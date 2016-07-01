9ab70c9fce23fbe5f9f739191d378b1fd6085abe
Abhilash Krishnan <abhilashk.cse@gmail.com>
2016-07-01 08:27:20 +0530

    Berlin Framework Version 0.1

---
+++ berlin-context/src/main/java/org/berlinframework/context/annotation/App.java
@@ -0,0 +1,16 @@
+package org.berlinframework.context.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Abhilash Krishnan
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface App {
+    String name() default "";
+    String displayName() default "";
+}
